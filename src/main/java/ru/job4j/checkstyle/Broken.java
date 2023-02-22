a96d7a7dddf60b63a8dee675c1feffa635ca07da
Dmitriy Kupreev <d3_october@yahoo.com>
2023-02-22 14:21:24 +0300

    Правила Checkstyle [#504981 #393975]_2

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -2,23 +2,20 @@ package ru.job4j.checkstyle;
 
 public class Broken {
     public static final String NEW_VALUE = "";
+    private int sizeOfEmpty = 10;
+    private String name;
+    private String surname;
 
-    private final int sizeOfEmpty = 10;
+    public Broken() { }
 
-    String name;
+    public void echo() { }
 
-    public String surname;
-
-    Broken() { }
-
-    void echo() { }
-
-    void media(Object obj) {
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) { }
+    public void method(int a, int b, int c, int d, int e, int f, int g) { }
 
 }

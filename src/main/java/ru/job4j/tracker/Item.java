b4a7f80c66316e9fdbee84ba82e4d7e18aaad3c6
Dmitriy Kupreev <d3_october@yahoo.com>
2023-02-24 18:32:30 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,11 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() { }
 
@@ -17,6 +20,10 @@ public class Item {
 
     public int getId() {
         return id;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return created;
     }
 
     public void setId(int id) {

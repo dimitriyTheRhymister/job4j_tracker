ae0cf1d0a35e639a612a4b8e8c7b95103d697d8e
Dmitriy Kupreev <d3_october@yahoo.com>
2023-02-25 21:46:54 +0300

    3.1. Массив с пустыми ячейками [#176470 #394925]_3

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -2,15 +2,15 @@ package ru.job4j.pojo;
 
 public class Shop {
     public static int indexOfNull(Product[] products) {
-        int result = -1;
+        int res = -1;
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
             if (product == null) {
-                result = i;
+                res = i;
                 break;
             }
         }
-        return result;
+        return res;
     }
 
     public static void main(String[] args) {

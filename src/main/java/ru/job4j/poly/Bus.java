0e3c2fadefa9d4c3e52b114f8dc43c795a40df7b
Dmitriy Kupreev <d3_october@yahoo.com>
2023-03-01 16:40:32 +0300

    4.2. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,15 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    public void drive() {
+        System.out.println("we are going");
+    }
+
+    public void passengers(int number) {
+        System.out.println("we have " +  number + "passengers");
+    }
+
+    public double refuel(double quantity) {
+        return 0;
+    }
+}

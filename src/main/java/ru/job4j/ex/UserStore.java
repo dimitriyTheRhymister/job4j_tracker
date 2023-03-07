5dfe28e833f41781d4f745f467183ad0c26dd050
Dmitriy Kupreev <d3_october@yahoo.com>
2023-03-07 11:44:27 +0300

    0.5. Иерархия исключений и множественный catch. [#219368 #398029]_2

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -6,6 +6,7 @@ public class UserStore {
         for (User user : users) {
             if (login.equals(user.getUsername())) {
                 rsl = user;
+                break;
             }
         }
         if (rsl == null) {
@@ -15,12 +16,10 @@ public class UserStore {
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        boolean rsl = true;
         if (user.getUsername().length() < 3 || !user.isValid()) {
-            rsl = false;
             throw new UserInvalidException("User '" + user.getUsername() + "' Invalid!");
         }
-        return rsl;
+        return true;
     }
 
     public static void main(String[] args) {

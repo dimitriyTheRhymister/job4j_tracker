ba27f29f7c6b210869804c751741d01bc52549f3
Dmitriy Kupreev <d3_october@yahoo.com>
2023-03-08 14:26:01 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #398353]_2

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -11,50 +11,44 @@ public class PasswordValidator {
         }
 
         boolean hasUpperCase = false;
-        for (int i = 0; i < password.length() && !hasUpperCase; i++) {
+        boolean hasSpecialSymbol = false;
+        boolean hasLowerCase = false;
+        boolean hasDigits = false;
+        for (int i = 0; i < password.length(); i++) {
             if (Character.isUpperCase(password.charAt(i))) {
                 hasUpperCase = true;
+            }
+            if (Character.isLowerCase(password.charAt(i))) {
+                hasLowerCase = true;
+            }
+            if (Character.isDigit(password.charAt(i))) {
+                hasDigits = true;
+            }
+            if (!Character.isLetterOrDigit(password.charAt(i))) {
+                hasSpecialSymbol = true;
             }
         }
         if (!hasUpperCase) {
             throw new IllegalArgumentException("Password should contain at least one uppercase letter");
         }
-
-        boolean hasLowerCase = false;
-        for (int i = 0; i < password.length() && !hasLowerCase; i++) {
-            if (Character.isLowerCase(password.charAt(i))) {
-                hasLowerCase = true;
-            }
-        }
         if (!hasLowerCase) {
             throw new IllegalArgumentException("Password should contain at least one lowercase letter");
         }
-
-        boolean hasDigits = false;
-        for (int i = 0; i < password.length() && !hasDigits; i++) {
-            if (Character.isDigit(password.charAt(i))) {
-                hasDigits = true;
-            }
-        }
         if (!hasDigits) {
             throw new IllegalArgumentException("Password should contain at least one figure");
-        }
-
-        boolean hasSpecialSymbol = false;
-        for (int i = 0; i < password.length() && !hasSpecialSymbol; i++) {
-            if (!Character.isLetterOrDigit(password.charAt(i))) {
-                hasSpecialSymbol = true;
-            }
         }
         if (!hasSpecialSymbol) {
             throw new IllegalArgumentException("Password should contain at least one special symbol");
         }
 
-        boolean hasSubstring = password.toLowerCase().contains("qwerty".toLowerCase())
-                || password.toLowerCase().contains("12345".toLowerCase())
-                || password.toLowerCase().contains("password".toLowerCase())
-                || password.toLowerCase().contains("admin".toLowerCase())
-                || password.toLowerCase().contains("user".toLowerCase());
+        boolean hasSubstring = false;
+        String[] substrings = {"qwerty", "12345", "password", "admin", "user"};
+        for (String substring : substrings) {
+            if (password.toLowerCase().contains(substring.toLowerCase())) {
+                hasSubstring = true;
+                break;
+            }
+        }
         if (hasSubstring) {
             throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
         }
@@ -63,6 +57,6 @@ public class PasswordValidator {
     }
 
     public static void main(String[] args) {
-        System.out.println(validate("rusrTr&rrr1245op"));
+        System.out.println(validate("ruTrr&rr1245op"));
     }
 }

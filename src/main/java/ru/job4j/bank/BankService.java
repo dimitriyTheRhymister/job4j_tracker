1c4bc38dbb4a95a32b3da71367693bab26bc389b
Dmitriy Kupreev <d3_october@yahoo.com>
2023-03-31 17:07:34 +0300

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -2,17 +2,43 @@ package ru.job4j.bank;
 
 import java.util.*;
 
+/**
+ * Класс описывает операции с клиентом в банке
+ *
+ * @author rhymister19@gmail.com
+ * @version 1.0
+ */
 public class BankService {
+    /**
+     * Коллекция типа HashMap - хранит список клиентов банка
+     */
     private final Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Метод - добавляет клиента в поле клиенты банка
+     *
+     * @param user - объект клиента для добавления
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<>());
     }
 
+    /**
+     * Метод - удаляет клиента из поля клиенты банка
+     *
+     * @param passport - паспорт клиента, которого нужно удалить
+     * @return возвращает удалённого клиента или null, если клиента с таким паспортом нет
+     */
     public boolean deleteUser(String passport) {
         return users.remove(new User(passport, "")) != null;
     }
 
+    /**
+     * Метод - добавляет счёт клиенту банка
+     *
+     * @param passport - паспорт клиента, которому нужно добавить счёт
+     * @param account  - счёт, который нужно добавить
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -23,6 +49,12 @@ public class BankService {
         }
     }
 
+    /**
+     * Метод - ищет клиента банка по паспорту
+     *
+     * @param passport - паспорт клиента, которого нужно найти
+     * @return возвращает найденного клиента или null, если клиента с таким паспортом нет
+     */
     public User findByPassport(String passport) {
         for (User keyUser : users.keySet()) {
             if (Objects.equals(keyUser.getPassport(), passport)) {
@@ -32,6 +64,13 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Метод - ищет счйт в банке по паспорту клиента и реквизиту счйта
+     *
+     * @param passport  - паспорт клиента, счйт которого нужно найти
+     * @param requisite - реквизит счёта, который нужно найти
+     * @return возвращает найденный счёт клиента или null, если такого счёта нет
+     */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -44,6 +83,15 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Метод - осуществляет перевод со счёта на счёт клиента в банке по номеру паспорта клиента и реквизиту счйта
+     * @param srcPassport   - паспорт клиента, с которого нужно перевести
+     * @param srcRequisite  - реквизит счёта, с которого нужно перевести
+     * @param destPassport  - паспорт клиента, на который нужно перевести
+     * @param destRequisite - реквизит счёта, на который нужно перевести
+     * @param amount - сколько средств нужно перевести
+     * @return возвращает true, если перевод прошёл успешно и false, если отсутствуют необходимые для перевода счет отправителя или получателя или если на счёте отправителя недостаточно средств для перевода
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         Account accountDest = findByRequisite(destPassport, destRequisite);
--- src/main/java/ru/job4j/bank/User.java
+++ src/main/java/ru/job4j/bank/User.java
@@ -2,27 +2,57 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает модель данных - клиент в банке
+ * @author rhymister19@gmail.com
+ * @version 1.0
+ */
 public class User {
+    /**
+     * Поле типа String - хранит № паспорта клиента
+     */
     private String passport;
+    /**
+     * Поле типа String - хранит имя клиента
+     */
     private String username;
 
+    /**
+     * Конструкторо класса - инициализирует поля класса
+     */
     public User(String passport, String username) {
         this.passport = passport;
         this.username = username;
     }
 
+    /**
+     * Метод - "геттер" - для получения значение типа String поля паспорт
+     * @return возвращает значение типа String поля паспорт
+     */
     public String getPassport() {
         return passport;
     }
 
+    /**
+     * Метод - "сеттер" - устанавливает значение типа String поля паспорт
+     * @param passport - строка для записи в поле паспорт
+     */
     public void setPassport(String passport) {
         this.passport = passport;
     }
 
+    /**
+     * Метод - "геттер" - для получения значение типа String поля имя
+     * @return возвращает значение типа String поля имя
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Метод - "сеттер" - устанавливает значение типа String поля имя
+     * @param username - строка для записи в поле имя
+     */
     public void setUsername(String username) {
         this.username = username;
     }

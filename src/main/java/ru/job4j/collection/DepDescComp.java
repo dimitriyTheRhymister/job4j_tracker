96dc4c9105cb2f627ad14d14c04e42bebcc45171
Dmitriy Kupreev <d3_october@yahoo.com>
2023-04-20 09:02:47 +0300

    3. Отсортировать департаменты [#34352 #410748]_4

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -1,11 +1,11 @@
 package ru.job4j.collection;
 
 import java.util.Comparator;
-import java.util.Objects;
 
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String s1, String s2) {
-        return Objects.equals(s1.split("/")[0], s2.split("/")[0]) ? s1.compareTo(s2) : s2.compareTo(s1);
+        int result = s2.split("/")[0].compareTo(s1.split("/")[0]);
+        return result != 0 ? result : s1.compareTo(s2);
     }
 }

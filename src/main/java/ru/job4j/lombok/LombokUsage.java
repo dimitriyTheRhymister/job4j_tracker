9bead1ab966c03df7abab1f83d8b08529c06f423
dimitriy <rhymister19@gmail.com>
2025-11-14 20:13:09 +0300

    3. @Builder [#504890]

--- src/main/java/ru/job4j/lombok/LombokUsage.java
+++ src/main/java/ru/job4j/lombok/LombokUsage.java
@@ -11,5 +11,25 @@ public class LombokUsage {
         var item = new ItemData();
         item.setId(1);
         System.out.println(item);
+
+        var role = Role.of()
+                .id(1)
+                .name("ADMIN")
+                .accessBy("create")
+                .accessBy("update")
+                .accessBy("read")
+                .accessBy("delete")
+                .build();
+        System.out.println(role);
+
+        var permission = Permission.of()
+                .id(1)
+                .name("ADMIN")
+                .rules("create")
+                .rules("update")
+                .rules("read")
+                .rules("delete")
+                .build();
+        System.out.println(permission);
     }
 }
